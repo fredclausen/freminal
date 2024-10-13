6acc08c17b7b5e85d5b25a8fa20536c68c5e54ab
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-10-13 09:36:12 -0600

    wip

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@
 #[macro_use]
 extern crate tracing;
 
+use smol_macros::main;
 use terminal_emulator::TerminalEmulator;
 use tracing::Level;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
@@ -71,43 +72,45 @@ impl Args {
     }
 }
 
-fn main() {
-    // use env for filtering
-    // example
-    // RUST_LOG=none,spectre_config=debug cargo run
+main! {
+    async fn main() {
+        // use env for filtering
+        // example
+        // RUST_LOG=none,spectre_config=debug cargo run
 
-    let env_filter = EnvFilter::builder()
-        .with_default_directive(Level::INFO.into())
-        .from_env_lossy()
-        .add_directive("winit=off".parse().unwrap())
-        .add_directive("wgpu=off".parse().unwrap())
-        .add_directive("eframe=off".parse().unwrap())
-        .add_directive("egui=off".parse().unwrap());
+        let env_filter = EnvFilter::builder()
+            .with_default_directive(Level::INFO.into())
+            .from_env_lossy()
+            .add_directive("winit=off".parse().unwrap())
+            .add_directive("wgpu=off".parse().unwrap())
+            .add_directive("eframe=off".parse().unwrap())
+            .add_directive("egui=off".parse().unwrap());
 
-    let subscriber = tracing_subscriber::registry().with(env_filter);
-    let fmt_layer = fmt::layer()
-        .with_line_number(true)
-        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ACTIVE)
-        .compact();
-    subscriber.with(fmt_layer).init();
+        let subscriber = tracing_subscriber::registry().with(env_filter);
+        let fmt_layer = fmt::layer()
+            .with_line_number(true)
+            .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ACTIVE)
+            .compact();
+        subscriber.with(fmt_layer).init();
 
-    trace!("Starting freminal");
-    debug!("Testing");
-    info!("Starting freminal");
+        trace!("Starting freminal");
+        debug!("Testing");
+        info!("Starting freminal");
 
-    let args = Args::parse(std::env::args());
-    let res = match TerminalEmulator::new(&args.recording) {
-        Ok(v) => gui::run(v),
-        Err(e) => {
-            error!(
-                "Failed to create terminal emulator: {}",
-                error::backtraced_err(&e)
-            );
-            return;
+        let args = Args::parse(std::env::args());
+        let res = match TerminalEmulator::new(&args.recording) {
+            Ok(v) => gui::run(v),
+            Err(e) => {
+                error!(
+                    "Failed to create terminal emulator: {}",
+                    error::backtraced_err(&e)
+                );
+                return;
+            }
+        };
+
+        if let Err(e) = res {
+            error!("Failed to run gui: {}", error::backtraced_err(&*e));
         }
-    };
-
-    if let Err(e) = res {
-        error!("Failed to run gui: {}", error::backtraced_err(&*e));
     }
 }
--- src/terminal_emulator/io/mod.rs
+++ src/terminal_emulator/io/mod.rs
@@ -4,17 +4,17 @@
 // https://opensource.org/licenses/MIT.
 
 mod pty;
-pub use pty::{CreatePtyIoError, FreminalPtyInputOutput};
+// pub use pty::{CreatePtyIoError, FreminalPtyInputOutput};
 
-pub type TermIoErr = Box<dyn std::error::Error>;
+// pub type TermIoErr = Box<dyn std::error::Error>;
 
-pub enum ReadResponse {
-    Success(usize),
-    Empty,
-}
+// pub enum ReadResponse {
+//     Success(usize),
+//     Empty,
+// }
 
-pub trait FreminalTermInputOutput {
-    fn read(&mut self, buf: &mut [u8]) -> Result<ReadResponse, TermIoErr>;
-    fn write(&mut self, buf: &[u8]) -> Result<usize, TermIoErr>;
-    fn set_win_size(&mut self, width: usize, height: usize) -> Result<(), TermIoErr>;
-}
+// pub trait FreminalTermInputOutput {
+//     fn read(&mut self, buf: &mut [u8]) -> Result<ReadResponse, TermIoErr>;
+//     fn write(&mut self, buf: &[u8]) -> Result<usize, TermIoErr>;
+//     fn set_win_size(&mut self, width: usize, height: usize) -> Result<(), TermIoErr>;
+// }
