6c787884d8be997f4ddfda2cd48bca08a4649830
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-11-14 11:02:48 -0700

    More internal tests

--- terminal-emulator/tests/internal.rs
+++ terminal-emulator/tests/internal.rs
@@ -12,7 +12,7 @@ use terminal_emulator::{
         buffer::TerminalBufferHolder,
         cursor::{CursorPos, CursorState},
         internal::{TerminalState, TERMINAL_HEIGHT, TERMINAL_WIDTH},
-        term_char::TChar,
+        term_char::{display_vec_tchar_as_string, TChar},
     },
 };
 
@@ -160,4 +160,111 @@ fn test_set_cursor_pos() {
     let cursor_pos = terminal_state.cursor_state.pos.clone();
     let expected = CursorPos { x: 0, y: 0 };
     assert_eq!(cursor_pos, expected);
+}
+
+#[test]
+fn test_set_cursor_pos_rel() {
+    let (tx, _rx) = crossbeam_channel::unbounded();
+    let mut terminal_state = TerminalState::new(tx.clone());
+    let data = b"Hello, World!";
+    terminal_state.handle_incoming_data(data);
+
+    // ESC [ Pn D
+    // "\0x1b[3D" moves the cursor right by 3
+    let data: [u8; 4] = [0x1b, 0x5b, 0x33, 0x44];
+    terminal_state.handle_incoming_data(&data);
+    let cursor_pos = terminal_state.cursor_state.pos.clone();
+    let expected = CursorPos { x: 10, y: 0 };
+    assert_eq!(cursor_pos, expected);
+}
+
+#[test]
+fn test_clear_display_from_cursor_to_end_of_display() {
+    let (tx, _rx) = crossbeam_channel::unbounded();
+    let mut terminal_state = TerminalState::new(tx.clone());
+    let data = b"Hello, World!";
+    terminal_state.handle_incoming_data(data);
+
+    // ESC [ Pn D
+    // "\0x1b[3D" moves the cursor right by 3
+    let data: [u8; 4] = [0x1b, 0x5b, 0x33, 0x44];
+    terminal_state.handle_incoming_data(&data);
+
+    // setup of buffer complete. Now we will clear the display from the cursor to the end of the display
+    // ESC [ Pn J
+    // "\0x1b[0J" clears the display from the cursor to the end of the display
+
+    let data: [u8; 4] = [0x1b, 0x5b, 0x30, 0x4a];
+    terminal_state.handle_incoming_data(&data);
+    let buffer = terminal_state.terminal_buffer.data();
+    let expected = TChar::from_vec(b"Hello, Wor\n").unwrap();
+    assert_eq!(buffer.visible, expected);
+}
+
+#[test]
+fn test_clear_display_from_start_of_display_to_cursor() {
+    let (tx, _rx) = crossbeam_channel::unbounded();
+    let mut terminal_state = TerminalState::new(tx.clone());
+    let data = b"Hello, World!";
+    terminal_state.handle_incoming_data(data);
+
+    // ESC [ Pn D
+    // "\0x1b[3D" moves the cursor right by 3
+    let data: [u8; 4] = [0x1b, 0x5b, 0x33, 0x44];
+    terminal_state.handle_incoming_data(&data);
+
+    // setup of buffer complete. Now we will clear the display from the start of the display to the cursor
+    // ESC [ Pn J
+    // "\0x1b[1J" clears the display from the start of the display to the cursor
+
+    let data: [u8; 4] = [0x1b, 0x5b, 0x31, 0x4a];
+    terminal_state.handle_incoming_data(&data);
+    let buffer = terminal_state.terminal_buffer.data();
+    let expected = TChar::from_vec(b"          ld!\n").unwrap();
+    assert_eq!(buffer.visible, expected);
+}
+
+#[test]
+fn test_clear_display() {
+    let (tx, _rx) = crossbeam_channel::unbounded();
+    let mut terminal_state = TerminalState::new(tx.clone());
+    terminal_state.set_win_size(30, 5);
+    // send the control code to clear the display
+    // ESC [ Pn J
+    // "\0x1b[3J" clears everything
+    let data: [u8; 4] = [0x1b, 0x5b, 0x33, 0x4a];
+    terminal_state.handle_incoming_data(&data);
+    let buffer = terminal_state.terminal_buffer.data();
+    // verify both the visible and scrollback buffers are empty
+    assert!(buffer.visible.is_empty());
+    assert!(buffer.scrollback.is_empty());
+
+    let data: [u8; 6] = [0x1b, 0x5b, 0x31, 0x3b, 0x31, 0x48];
+    terminal_state.handle_incoming_data(&data);
+
+    let data = b"Hello, World!\n\rHello, World!\n\rHello, World!\n\rHello, World!\n\rHello, World!\n\rHello, World!\n\r";
+    terminal_state.handle_incoming_data(data);
+
+    // ensure the scrollback and visible buffers are are correct
+    let buffer = terminal_state.terminal_buffer.data();
+    let expected_visible =
+        b"Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n";
+    let expected_visible = TChar::from_vec(expected_visible).unwrap();
+    let scrollback_expected = b"Hello, World!\n";
+    let scrollback_expected = TChar::from_vec(scrollback_expected).unwrap();
+
+    assert_eq!(
+        buffer.visible,
+        expected_visible,
+        "\nvisible buffer:\n{}\nexpected:\n{}",
+        display_vec_tchar_as_string(&buffer.visible),
+        display_vec_tchar_as_string(&expected_visible)
+    );
+    assert_eq!(
+        buffer.scrollback,
+        scrollback_expected,
+        "\nscrollback buffer:\n{}\nexpected:\n{}",
+        display_vec_tchar_as_string(&buffer.scrollback),
+        display_vec_tchar_as_string(&scrollback_expected)
+    );
 }
