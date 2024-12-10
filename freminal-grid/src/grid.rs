f9d5179c32d336a589a4867896c8ca2b1f2cec01
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-12-10 14:23:19 -0700

    wip

---
+++ freminal-grid/src/grid.rs
@@ -0,0 +1,20 @@
+// Copyright (C) 2024 Fred Clausen
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+use crate::line::{Cell, Line};
+
+pub struct Grid<T> {
+    inner: Vec<Line<T>>,
+    max_height: usize,
+}
+
+impl<T: Cell> Grid<T> {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            inner: (0..height).map(|_| Line::new(width)).collect(),
+            max_height: height,
+        }
+    }
+}
