12c901dd755df55d43376343e93524e482be4d6e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-12-25 01:01:55 -0700

    wip

---
+++ freminal-grid/src/cursor.rs
@@ -0,0 +1,12 @@
+// Copyright (C) 2024 Fred Clausen
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+pub trait Cursor<U, P>: Sized + Default + Clone + Eq + PartialEq {
+    fn current_position(&self) -> P;
+    fn current_decorations(&self) -> U;
+}
+
+pub trait Position {}
+pub trait Decorations {}
