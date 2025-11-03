6d4d43fa3dafcea3db5260be0b3d7de319dfdd5a
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-03 10:19:05 -0700

    pre-commit

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # freminal
 
 [![codecov](https://codecov.io/gh/fredclausen/freminal/graph/badge.svg?token=H03IXCMD1Y)](https://codecov.io/gh/fredclausen/freminal)
-[![made-with-rust](https://img.shields.io/badge/Made%20with-Rust-1f425f.svg)](https://www.rust-lang.org/)
 [![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/fredclausen/freminal/LICENSE)
 [![GitHub issues](https://img.shields.io/github/issues/Naereen/StrapDown.js.svg)](https://github.com/fredclausen/freminal/issues/)
 [![GitHub pull-requests](https://img.shields.io/github/issues-pr/Naereen/StrapDown.js.svg)](https://GitHub.com/fredclausen/freminal/pull/)
+[![made-with-rust](https://img.shields.io/badge/Made%20with-Rust-1f425f.svg)](https://www.rust-lang.org/)
+
+````
+
+```bash
+
+
+
+```shell
+
+Alternatively, if you have pre-commit installed, you can run the following command:
+
+And ensure that all tests pass. If you're adding a new feature, please add tests for it. If you're fixing a bug, please add a test that would have caught the bug. And lastly, please ensure your commits are signed.
+
+As of now, I would consider this mostly stable, but hardly performant. Please see the [TODO](TODO.md) for a list of features that are planned. Please see [Supported Control Codes](SUPPORTED_CONTROL_CODES.md) for a list of control codes that are currently supported.
+
+Before you submit a PR, please run the following commands:
+cargo install cargo-docs-rs typos-cli cargo-deny cargo-xtask
+cargo xtask ci
+
+## SSH or why does the terminal act weird?
+
+For remote sessions, like SSH, you may see odd things like duplicated input characters and such. This is because there are no termcaps installed on the remote host for freminal. Maybe, eventually, the termcaps can be included in distributions, but in the mean time the following command (run inside of freminal) will install the termcaps on the remote host:
 
 ## Description
 
@@ -12,41 +34,15 @@ Freminal (or "Fred's Terminal") is a terminal emulator, written in Rust, that th
 
 Freminal is meant to emulate the old VT220s; however it supports (or will support) modern xterm control codes, unicode, and other modern features.
 
-As of now, I would consider this mostly stable, but hardly performant. Please see the [TODO](TODO.md) for a list of features that are planned. Please see [Supported Control Codes](SUPPORTED_CONTROL_CODES.md) for a list of control codes that are currently supported.
-
-## SSH or why does the terminal act weird?
-
-For remote sessions, like SSH, you may see odd things like duplicated input characters and such. This is because there are no termcaps installed on the remote host for freminal. Maybe, eventually, the termcaps can be included in distributions, but in the mean time the following command (run inside of freminal) will install the termcaps on the remote host:
-
-```shell
+If you don't have the following tools installed, you can install them with the following commands:
 infocmp -x | ssh YOUR-SERVER -- tic -x -
-```
+
+OR if you have installed the git hooks for the repo clippy, fmt and the ci xtask are run and everything will be good.
+pre-commit run --all-files
 
 ## Contributing
 
 PRs are welcome. To make sure that your PR is accepted, there are a few things I ask.
-
-If you don't have the following tools installed, you can install them with the following commands:
-
-```bash
-cargo install cargo-docs-rs typos-cli cargo-deny cargo-xtask
-```
-
-Before you submit a PR, please run the following commands:
-
-```bash
-cargo xtask ci
-```
-
-Alternatively, if you have pre-commit installed, you can run the following command:
-
-```bash
-pre-commit run --all-files
-```
-
-OR if you have installed the git hooks for the repo clippy, fmt and the ci xtask are run and everything will be good.
-
-And ensure that all tests pass. If you're adding a new feature, please add tests for it. If you're fixing a bug, please add a test that would have caught the bug. And lastly, please ensure your commits are signed.
 
 ## Credit
 
--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
+  description = "Freminal development environment with reproducible pre-commit hooks";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    systems.url = "github:nix-systems/default";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
@@ -11,57 +15,270 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
+      systems,
+      git-hooks,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      checks = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
 
-        libPath = pkgs.lib.makeLibraryPath (
-          with pkgs;
-          [
-            libGL
-            libxkbcommon
-          ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ wayland ]
-        );
+          # --- pre-commit-hooks (from GitHub, PyPI tarball is gone)
+          preCommitHooksPkg = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "pre-commit-hooks";
+            version = "5.0.0";
+            pyproject = true;
+            build-system = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.wheel
+            ];
+            src = pkgs.fetchFromGitHub {
+              owner = "pre-commit";
+              repo = pname;
+              rev = "${version}";
+              sha256 = "sha256-BYNi/xtdichqsn55hqr1MSFwWpH+7cCbLfqmpn9cxto=";
+            };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "llvm-tools-preview"
-          ];
-        };
+            propagatedBuildInputs = [ ruamelYaml_0186 ];
+          };
 
-        nativeBuildInputs = with pkgs; [ rustToolchain ];
-        buildInputs = with pkgs; [
-          cargo-make
-          cargo-deny
-          cargo-machete
-          cargo-profiler
-          samply
-          cargo-tauri
-          typos
-          #cargo-llvm-cov
-        ];
+          # --- codespell
+          codespellPkg = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "codespell";
+            version = "2.4.1";
+            pyproject = true;
+            build-system = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.setuptools-scm
+            ];
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-KZ/NywnSPoHjWmcbvnRtWtfoOFly5l27gzouqsM8AeU=";
+            };
+          };
 
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-          shellHook = ''
-                    export RUST_SRC_PATH=${RUST_SRC_PATH}
-                    export LD_LIBRARY_PATH=${libPath}:$LD_LIBRARY_PATH
+          # --- pinned ruamel.yaml 0.18.6 (runtime dep)
+          ruamelYaml_0186 = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "ruamel.yaml";
+            version = "0.18.6";
+            pyproject = true;
+            build-system = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.wheel
+            ];
+            src = pkgs.fetchPypi {
+              pname = "ruamel.yaml";
+              inherit version;
+              sha256 = "sha256-iyfmohfnhsb75WNNjz8RvGPg+A9qWJDyiGPZxFqsMRs=";
+            };
+          };
 
-                    # Install cargo-llvm-cov if not already present
-            if ! command -v cargo-llvm-cov >/dev/null 2>&1; then
-              echo "Installing cargo-llvm-cov into local Cargo bin..."
-              cargo install cargo-llvm-cov --locked
-            fi
+          # --- check-jsonschema (validated from GitHub)
+          checkJsonschema = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "check-jsonschema";
+            version = "0.29.4";
+            pyproject = true;
+            build-system = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.wheel
+            ];
+            src = pkgs.fetchFromGitHub {
+              owner = "sirosen";
+              repo = pname;
+              rev = "${version}";
+              sha256 = "sha256-JSz8zwiOnX3nuKypffe0wZ7YoZ7uHH/lAoUgdKfkEO8=";
+            };
+            propagatedBuildInputs =
+              with pkgs.python3Packages;
+              [
+                click
+                requests
+                jsonschema
+                regress
+              ]
+              ++ [ ruamelYaml_0186 ];
+          };
+        in
+        {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+
+            hooks = {
+              # --- YAML / whitespace / shebangs ---
+              check-yaml = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/check-yaml";
+              };
+              end-of-file-fixer = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/end-of-file-fixer";
+              };
+              trailing-whitespace = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/trailing-whitespace-fixer";
+              };
+              requirements-txt-fixer = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/requirements-txt-fixer";
+              };
+              mixed-line-ending = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/mixed-line-ending";
+                args = [ "--fix=auto" ];
+              };
+              check-executables-have-shebangs = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/check-executables-have-shebangs";
+              };
+              check-shebang-scripts-are-executable = {
+                enable = true;
+                entry = "${preCommitHooksPkg}/bin/check-shebang-scripts-are-executable";
+              };
+
+              # --- Dockerfile linting ---
+              hadolint.enable = true;
+
+              # --- Prettier ---
+              prettier = {
+                enable = true;
+                types_or = [
+                  "file"
+                  "bash"
+                  "sh"
+                  "javascript"
+                  "jsx"
+                  "ts"
+                  "tsx"
+                ];
+                extraPackages = [ pkgs.nodePackages.prettier ];
+              };
+
+              # --- Codespell ---
+              codespell = {
+                enable = true;
+                entry = "${codespellPkg}/bin/codespell";
+                args = [ "--ignore-words=.dictionary.txt" ];
+                files = "\\.([ch]|cpp|rs|py|sh|txt|md|toml|yaml|yml)$";
+              };
+
+              # --- Shell scripts ---
+              shellcheck.enable = true;
+
+              # --- GitHub Actions / Workflows validation ---
+              check-github-actions = {
+                enable = true;
+                entry = "${checkJsonschema}/bin/check-jsonschema";
+                args = [
+                  "--check-type"
+                  "github-actions"
+                ];
+                files = "\\.ya?ml$";
+              };
+              check-github-workflows = {
+                enable = true;
+                entry = "${checkJsonschema}/bin/check-jsonschema";
+                args = [
+                  "--check-type"
+                  "github-workflows"
+                ];
+                files = "\\.ya?ml$";
+              };
+
+              # --- Rust ---
+              rustfmt.enable = true;
+              clippy.enable = true;
+
+              # --- Python ---
+              black.enable = true;
+              flake8 = {
+                enable = true;
+                args = [ "--extend-ignore=W503,W504,E501" ];
+              };
+
+              # --- Nix ---
+              nixfmt.enable = true;
+            };
+          };
+        }
+      );
+
+      # --- Formatter helper (run all hooks)
+      formatter = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (self.checks.${system}.pre-commit-check) config;
+          inherit (config) package configFile;
+          script = ''
+            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
           '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
+
+      # --- Expose as an app (so `nix run .#pre-commit-run` works)
+      apps = eachSystem (system: {
+        pre-commit-run = {
+          type = "app";
+          program = "${self.formatter.${system}}/bin/pre-commit-run";
         };
-      }
-    );
+      });
+
+      # --- Dev shell ---
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "llvm-tools-preview"
+            ];
+          };
+
+          libPath = pkgs.lib.makeLibraryPath (
+            with pkgs;
+            [
+              libGL
+              libxkbcommon
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ wayland ]
+          );
+
+          rustTools = with pkgs; [
+            cargo-make
+            cargo-deny
+            cargo-machete
+            cargo-profiler
+            samply
+            cargo-tauri
+            typos
+          ];
+
+          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ rustToolchain ] ++ rustTools ++ enabledPackages;
+            shellHook = ''
+              ${shellHook}
+              alias pre-commit="pre-commit-run"
+              export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
+              export LD_LIBRARY_PATH=${libPath}:$LD_LIBRARY_PATH
+            '';
+          };
+        }
+      );
+    };
 }
