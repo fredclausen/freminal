8c965124ff5ab8732fe91d5b008b809f860382d5
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-02 17:52:19 -0700

    fix flake formatting

--- flake.nix
+++ flake.nix
@@ -5,22 +5,34 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
         libPath = pkgs.lib.makeLibraryPath (
-          with pkgs; [
+          with pkgs;
+          [
             libGL
             libxkbcommon
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ wayland ]
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ wayland ]
         );
 
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-  extensions = [ "rust-src" "llvm-tools-preview" ];
-};
+          extensions = [
+            "rust-src"
+            "llvm-tools-preview"
+          ];
+        };
 
         nativeBuildInputs = with pkgs; [ rustToolchain ];
         buildInputs = with pkgs; [
@@ -40,15 +52,16 @@
         devShells.default = pkgs.mkShell {
           inherit buildInputs nativeBuildInputs;
           shellHook = ''
-            export RUST_SRC_PATH=${RUST_SRC_PATH}
-            export LD_LIBRARY_PATH=${libPath}:$LD_LIBRARY_PATH
+                    export RUST_SRC_PATH=${RUST_SRC_PATH}
+                    export LD_LIBRARY_PATH=${libPath}:$LD_LIBRARY_PATH
 
-            # Install cargo-llvm-cov if not already present
-    if ! command -v cargo-llvm-cov >/dev/null 2>&1; then
-      echo "Installing cargo-llvm-cov into local Cargo bin..."
-      cargo install cargo-llvm-cov --locked
-    fi
+                    # Install cargo-llvm-cov if not already present
+            if ! command -v cargo-llvm-cov >/dev/null 2>&1; then
+              echo "Installing cargo-llvm-cov into local Cargo bin..."
+              cargo install cargo-llvm-cov --locked
+            fi
           '';
         };
-      });
+      }
+    );
 }
